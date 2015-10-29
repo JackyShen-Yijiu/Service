2142ab7c8f6c937d7487f838a5b90bf188b28be3
v-yafeili <v-yafeili@outlook.com>
2015-10-29 20:24:24 +0800

    修改自动刷新数据库

--- BlackCatServer/Common/refreshstate.js
+++ BlackCatServer/Common/refreshstate.js
@@ -10,9 +10,11 @@ var rule = new schedule.RecurrenceRule();
 var mongodb = require('../models/mongodb.js');
 var reservationmodel=mongodb.ReservationModel;
 var appTypeEmun=require("../custommodel/emunapptype");
+require('date-utils');
 var times = [];
 
-for(var i=1; i<60; i=i+5){
+
+for(var i=1; i<60; i=i+1){
   //  console.log(i);
     times.push(i);
 
@@ -22,15 +24,29 @@ rule.minute = times;
 
 try{
 var j = schedule.scheduleJob(rule, function(){
-
-    console.log(new Date().toLocaleDateString()+": 开始更新预约状态");
+   // console.log((new Date()).addMinutes(-30));
+    console.log(new Date().toString()+": 开始更新预约状态");
     reservationmodel.update({reservationstate:appTypeEmun.ReservationState.applyconfirm,endtime:{ "$lt": new Date()}} ,
-        { $set: { reservationstate:appTypeEmun.ReservationState.unconfirmfinish }},{safe: false, multi: true});
+        { $set: { reservationstate:appTypeEmun.ReservationState.unconfirmfinish }},{safe: false, multi: true},function(err,doc){
+            console.log(new Date().toString()+": 更新待评价状态结果：");
+            if(err){
+                console.log(err)
+            }
+            console.log(doc)
+        });
 // 自动修改
-    reservationmodel.update({reservationstate:appTypeEmun.ReservationState.applying,reservationcreatetime:{ "$gt": new Date().addMinute(-10)}} ,
-        { $set: { reservationstate:appTypeEmun.ReservationState.applyconfirm }},{safe: false, multi: true})
-    console.log(new Date().toLocaleDateString()+": 更新预约状态,完成");
+
+    reservationmodel.update({reservationstate:appTypeEmun.ReservationState.applying,reservationcreatetime:{ "$gt": new Date().addMinutes(-100)}} ,
+        { $set: { reservationstate:appTypeEmun.ReservationState.applyconfirm }},{safe: false, multi: true},function(err,doc){
+            console.log(new Date().toString()+": 订单确认状态结果：");
+            if(err){
+                console.log(err)
+            }
+            console.log(doc)
+        })
+    console.log(new Date().toString()+": 更新预约状态,完成");
+    console.log("-----------------------------------------------------------------------------------------------------------------");
 });
 } catch(e){
-       console.log(new Date().toLocaleDateString()+'更新预约状态error..'+ e.message);
+       console.log(new Date().toString()+'更新预约状态error..'+ e.message);
      }
