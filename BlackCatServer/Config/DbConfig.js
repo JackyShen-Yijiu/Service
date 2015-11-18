386c6535a5c5398568dd033665d0465d54f7c543
v-yafeili <v-yafeili@outlook.com>
2015-11-18 23:26:55 +0800

    正式版发布

--- BlackCatServer/Config/DbConfig.js
+++ BlackCatServer/Config/DbConfig.js
@@ -6,8 +6,8 @@ module.exports = {
 
     db: {
         //uri: process.env.MONGODB || 'mongodb://101.200.204.240/BlackCat',
-       // uri: process.env.MONGODB || 'mongodb://10.171.52.27/BlackCat_Prod',
-        uri: process.env.MONGODB || 'mongodb://123.57.7.30/BlackCat',
+        uri: process.env.MONGODB || 'mongodb://10.171.52.27/BlackCat_Prod',
+       //uri: process.env.MONGODB || 'mongodb://123.57.7.30/BlackCat',
         //uri: process.env.MONGODB || 'mongodb://10.171.52.27/BlackCat',
         //uri: process.env.MONGODB || 'mongodb://192.168.7.101/BlackCat',
         options: {
