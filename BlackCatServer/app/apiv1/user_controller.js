d9358133d40de6eaca3168ac2f83a0c2f735bb42
v-yafeili <v-yafeili@outlook.com>
2015-11-26 10:10:15 +0800

    测试购买商品

--- BlackCatServer/app/apiv1/user_controller.js
+++ BlackCatServer/app/apiv1/user_controller.js
@@ -79,6 +79,8 @@ exports.userBuyProduct=function(req,res){
         mobile:req.body.mobile,
         address:req.body.address,
     }
+    console.log(postinfo);
+    console.log(req.body);
     if (postinfo.usertype === undefined
         ||postinfo.userid === undefined
         ||postinfo.productid === undefined ) {
