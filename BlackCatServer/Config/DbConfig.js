d91dc618344f293cdf41088f36576c394fe031b8
v-yafeili <v-yafeili@outlook.com>
2016-02-02 17:48:26 +0800

    修改科目试题

--- BlackCatServer/Config/DbConfig.js
+++ BlackCatServer/Config/DbConfig.js
@@ -6,12 +6,12 @@ module.exports = {
 
     db: {
         //uri: process.env.MONGODB || 'mongodb://123.57.7.30:27300/BlackCat_Prod',
-        uri: process.env.MONGODB || 'mongodb://123.57.7.30:27300/BlackCat_Prod',
+       // uri: process.env.MONGODB || 'mongodb://123.57.7.30:27300/BlackCat_Prod',
 
 
          //uri: process.env.MONGODB || 'mongodb://123.57.7.30:27300/BlackCat',
 
-       //uri: process.env.MONGODB || 'mongodb://101.200.204.240:27017/jxmisdb',
+       uri: process.env.MONGODB || 'mongodb://101.200.204.240:27017/jxmisdb',
 
         //uri: process.env.MONGODB || 'mongodb://123.57.7.30:27300/BlackCat',
 
