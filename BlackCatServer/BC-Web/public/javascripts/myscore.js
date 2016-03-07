eca14fe76598ed68116284deb6e19acea05ee234
1186823079@qq.com <liu19921992316>
2016-03-07 18:28:24 +0800

    score

--- BlackCatServer/BC-Web/public/javascripts/myscore.js
+++ BlackCatServer/BC-Web/public/javascripts/myscore.js
@@ -1,11 +1,97 @@
 (function(){
 	// http://localhost:3600/question/getmysocre?userid=56c129e11bedd418084c043b
-    $.get('http://localhost:3600/question/getmysocre?userid=56c129e11bedd418084c043b', function(Mydata){
-        console.log(Mydata.data);
-        console.log(Mydata.data.kemusi_score);//科四
-        console.log(Mydata.data.kemuyi_score);//
+    //获取数据
+    $.get('http://192.168.1.105:3600/question/getmysocre?userid=56c129e11bedd418084c043b', function(Mydata){
+        //console.log(Mydata.data);
+        //console.log(Mydata.data.kemusi_score);//科四
+        //console.log(Mydata.data.kemuyi_score);//科目一
+        //console.log(Mydata.data.kemuyi_score[i].socre);
+        var oScoreBlock=$('.score-block')[0];
+
+        for (var i = 0; i < Mydata.data.kemuyi_score.length; i++) {
+            var div=document.createElement('div');
+
+            var spans="";
+            //毫秒转换成本地时间
+            /*date start*/
+            var date_start=new Date(Mydata.data.kemuyi_score[i].begintime);
+            var date_end=new Date(Mydata.data.kemuyi_score[i].endtime);
+
+            var year_start=date_start.getFullYear();
+            var year_end=date_end.getFullYear();
+
+            var month_start=date_start.getMonth()+1;
+            var month_end=date_end.getMonth()+1;
+
+            var day_start=date_start.getDay()+1;
+            var day_end=date_end.getDay()+1;
+
+            var date_start=year_start+"/"+month_start+"/"+day_start;
+            var date_end=year_end+"/"+month_end+"/"+day_end;
+            /*date end*/
+
+            spans+="<span class='cell-title'>"+"得分："+Mydata.data.kemuyi_score[i].socre+"</span>";
+            spans+="<span class='cell-title'>"+"开始时间："+date_start+"</span>";
+            spans+="<span class='cell-title'>"+"结束时间："+date_end+"</span>";
+
+            if(Mydata.data.kemuyi_score[i].is_pass==1){
+                spans+="<span class='cell-title'>"+"是否通过：是"+"</span>";
+            }else{
+                spans+="<span class='cell-title'>"+"是否通过：否"+"</span>";
+            }
+
+
+            div.innerHTML=spans;
+
+            oScoreBlock.appendChild(div);
+
+        };
+
+        var oScoreBlock=$('.score-block')[1];
+
+        for (var i = 0; i < Mydata.data.kemuyi_score.length; i++) {
+            var div=document.createElement('div');
+
+            var spans="";
+
+            /*date start*/
+            var date_start=new Date(Mydata.data.kemuyi_score[i].begintime);
+            var date_end=new Date(Mydata.data.kemuyi_score[i].endtime);
+
+            var year_start=date_start.getFullYear();
+            var year_end=date_end.getFullYear();
+
+            var month_start=date_start.getMonth()+1;
+            var month_end=date_end.getMonth()+1;
+
+            var day_start=date_start.getDay()+1;
+            var day_end=date_end.getDay()+1;
+
+            var date_start=year_start+"/"+month_start+"/"+day_start;
+            var date_end=year_end+"/"+month_end+"/"+day_end;
+            /*date end*/
+
+            spans+="<span class='cell-title'>"+"得分："+Mydata.data.kemuyi_score[i].socre+"</span>";
+            spans+="<span class='cell-title'>"+"开始时间："+date_start+"</span>";
+            spans+="<span class='cell-title'>"+"结束时间："+date_end+"</span>";
+
+            if(Mydata.data.kemuyi_score[i].is_pass==1){
+                spans+="<span class='cell-title'>"+"是否通过：是"+"</span>";
+            }else{
+                spans+="<span class='cell-title'>"+"是否通过：否"+"</span>";
+            }
+
+            div.innerHTML=spans;
+
+            oScoreBlock.appendChild(div);
+
+        };
+
+
+
+
 
     });
 
-	
+
 })();
