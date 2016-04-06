abf5124006bb20f43f963d75a933875c6201b09b
v-yafeili <v-yafeili@outlook.com>
2016-04-06 11:51:26 +0800

    增加用户报名

--- BlackCatServer/app/apiv1/course_controller.js
+++ BlackCatServer/app/apiv1/course_controller.js
@@ -91,7 +91,7 @@ exports.postReservation=function(req,res){
             new BaseReturnInfo(0,"课程不能为空",""));
     }
     var now=new Date();
-    if(now-(new Date(begintime))>0){
+    if(now-(new Date(reservationinfo.begintime))>0){
         return res.json(new BaseReturnInfo(0,"该时段不能预约",[]));
     }
     courseserver.postReservation(reservationinfo,function(err,data){
