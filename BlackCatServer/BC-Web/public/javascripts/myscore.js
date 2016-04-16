5fec7f1723aa91fd3f1928b913c0b867c4435729
moodpo <moodpo@outlook.com>
2016-04-16 16:40:29 +0800

    修改我的成绩单页面错误

--- BlackCatServer/BC-Web/public/javascripts/myscore.js
+++ BlackCatServer/BC-Web/public/javascripts/myscore.js
@@ -1,55 +1,74 @@
 (function(){
 	// http://localhost:3600/question/getmysocre?userid=56c129e11bedd418084c043b
 
+    function getUrlParam(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        if (r != null) return unescape(r[2]); return null; //返回参数值
+    }
+
+    var userid = getUrlParam('userid');
+
     //获取数据
-    $.get('http://www.yibuxueche.com/question/getmysocre?userid=56c129e11bedd418084c043b', function(Mydata){
+    $.get('http://www.yibuxueche.com/question/getmysocre?userid=' + userid, function(Mydata){
         console.log(Mydata.data);
         //console.log(Mydata.data.kemusi_score);//科四
         //console.log(Mydata.data.kemuyi_score);//科目一
         //console.log(Mydata.data.kemuyi_score[i].socre);
-        var oScoreBlock=$('.score-block')[0];
+        var oScoreBlock01 = $('#scoreBlock01');
+        var oScoreBlock02 = $('#scoreBlock02');
 
-        for (var i = 0; i < Mydata.data.kemuyi_score.length; i++) {
-            var div=document.createElement('div');
+        if(Mydata.data.kemuyi_score.length > 0) {
+            oScoreBlock01.show();
+            oScoreBlock02.hide();
+        }
+        if(Mydata.data.kemusi_score.length > 0) {
+            oScoreBlock01.hide();
+            oScoreBlock02.show();
+        }
 
-            var spans="";
-            //毫秒转换成本地时间
-            /*date start*/
-            var date_start=new Date(Mydata.data.kemuyi_score[i].begintime*1000);
-            var date_end=new Date(Mydata.data.kemuyi_score[i].endtime*1000);
+        (function(){
+            for (var i = 0; i < Mydata.data.kemuyi_score.length; i++) {
+                var div=document.createElement('div');
 
-            var year_start=date_start.getFullYear();
-            var year_end=date_end.getFullYear();
+                var spans="";
+                //毫秒转换成本地时间
+                /*date start*/
+                var date_start=new Date(Mydata.data.kemuyi_score[i].begintime*1000);
+                var date_end=new Date(Mydata.data.kemuyi_score[i].endtime*1000);
 
-            var month_start=date_start.getMonth()+1;
-            var month_end=date_end.getMonth()+1;
+                var year_start=date_start.getFullYear();
+                var year_end=date_end.getFullYear();
 
-            var day_start=date_start.getDay()+1;
-            var day_end=date_end.getDay()+1;
+                var month_start=date_start.getMonth()+1;
+                var month_end=date_end.getMonth()+1;
 
-            var date_start=year_start+"/"+month_start+"/"+day_start;
-            var date_end=year_end+"/"+month_end+"/"+day_end;
-            /*date end*/
+                var day_start=date_start.getDay()+1;
+                var day_end=date_end.getDay()+1;
 
-            spans+="<span class='cell-title'>"+"得分："+Mydata.data.kemuyi_score[i].socre+"</span>";
-            spans+="<span class='cell-title'>"+"开始时间："+date_start+"</span>";
-            spans+="<span class='cell-title'>"+"结束时间："+date_end+"</span>";
+                var date_start=year_start+"/"+month_start+"/"+day_start;
+                var date_end=year_end+"/"+month_end+"/"+day_end;
+                /*date end*/
 
-            if(Mydata.data.kemuyi_score[i].is_pass==1){
-                spans+="<span class='cell-title'>"+"是否通过：是"+"</span>";
-            }else{
-                spans+="<span class='cell-title'>"+"是否通过：否"+"</span>";
-            }
+                spans+="<span class='cell-title'>"+"得分："+Mydata.data.kemuyi_score[i].socre+"</span>";
+                spans+="<span class='cell-title'>"+"开始时间："+date_start+"</span>";
+                spans+="<span class='cell-title'>"+"结束时间："+date_end+"</span>";
+
+                if(Mydata.data.kemuyi_score[i].is_pass==1){
+                    spans+="<span class='cell-title'>"+"是否通过：是"+"</span>";
+                }else{
+                    spans+="<span class='cell-title'>"+"是否通过：否"+"</span>";
+                }
 
 
-            div.innerHTML=spans;
+                div.innerHTML=spans;
 
-            oScoreBlock.appendChild(div);
+                oScoreBlock01.appendChild(div);
 
-        };
+            };
+        })();
 
-        var oScoreBlock=$('.score-block')[1];
-
+        
         for (var i = 0; i < Mydata.data.kemusi_score.length; i++) {
             var div=document.createElement('div');
 
@@ -84,7 +103,7 @@
 
             div.innerHTML=spans;
 
-            oScoreBlock.appendChild(div);
+            oScoreBlock02.appendChild(div);
 
         };
 
